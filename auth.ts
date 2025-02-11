44a319b8e91d981f3e4286a10fbacfe345182758
Daniel Van Cuylenburg <email@danielvanc.com>
2025-02-11 21:13:28 +0000

    fix: update redirectProxyUrl

--- auth.ts
+++ auth.ts
@@ -7,5 +7,5 @@ const NEXTAUTH_URL = process.env.VERCEL_URL
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
-  redirectProxyUrl: NEXTAUTH_URL + "/api/auth/",
+  redirectProxyUrl: NEXTAUTH_URL + "/api/auth",
 });
