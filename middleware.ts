8f1985b385447ba65bdcfdef9ed5c800e3d54a97
Daniel Van Cuylenburg <email@danielvanc.com>
2025-02-11 21:29:58 +0000

    fix: update middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,9 @@ const protectedRoutes = "/home";
 
 export default auth((req) => {
   const path = req.nextUrl.pathname;
-  const sessionCookie = req.cookies.get("authjs.session-token");
+  const sessionCookie =
+    req.cookies.get("authjs.session-token") ||
+    req.cookies.get("__Secure-authjs.session-token");
   const isProtectedRoute = path.startsWith(protectedRoutes);
 
   if (isProtectedRoute && !sessionCookie)
