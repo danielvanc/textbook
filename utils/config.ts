f0d33b2e7fea8cbe5b8b42aff6972f0414ad8b81
Daniel Van Cuylenburg <email@danielvanc.com>
2025-02-12 09:45:36 +0000

    fix: update session cookie handling in middleware

--- utils/config.ts
+++ utils/config.ts
@@ -2,6 +2,9 @@ const config = {
   appRoute: "/home",
   loginRoute: "/login",
   logoutRoute: "/logout",
+  sessionCookie: !!process.env.VERCEL_URL
+    ? "__Secure-authjs.session-token"
+    : "authjs.session-token",
 };
 
 export default config;
