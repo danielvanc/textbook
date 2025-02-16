3e535c66d745beabefacb741ff1234006a715c3b
Daniel Van Cuylenburg <email@danielvanc.com>
2025-02-16 20:16:53 +0000

    feat: redirect logged in users

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,12 @@
 import { logInUser } from "@/app/actions";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function SignIn() {
+export default async function SignIn() {
+  const session = await auth();
+
+  if (session) redirect("/home");
+
   return (
     <form action={logInUser}>
       <button type="submit">Signin with Google</button>
