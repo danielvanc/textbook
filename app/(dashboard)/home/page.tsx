1b7bab4e68b731837a90c15ce713cd457029f182
Daniel Van Cuylenburg <email@danielvanc.com>
2025-02-17 15:21:45 +0000

    fix: remove logs

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -5,8 +5,6 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await auth();
 
-  console.log("session", session);
-
   if (!session) redirect("/login");
 
   return (
