b502959810f40a35f208e6c85f3efeb4c9ef5b8e
Daniel Van Cuylenburg <email@danielvanc.com>
2025-03-06 17:19:09 +0000

    feat: introduced prisma optimize

--- utils/db.ts
+++ utils/db.ts
@@ -2,46 +2,15 @@ import { remember } from "@epic-web/remember";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { withOptimize } from "@prisma/extension-optimize";
-import chalk from "chalk";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { notFound, redirect } from "next/navigation";
 
-const prismaClientSingleton = () => {
-  // Feel free to change this log threshold to something that makes sense for you
-  const logThreshold = 20;
-
-  const client = new PrismaClient({
-    log: [
-      { level: "query", emit: "event" },
-      { level: "error", emit: "stdout" },
-      { level: "warn", emit: "stdout" },
-    ],
-  });
-
-  client.$on("query", async (e) => {
-    if (e.duration < logThreshold) return;
-    const color =
-      e.duration < logThreshold * 1.1
-        ? "green"
-        : e.duration < logThreshold * 1.2
-        ? "blue"
-        : e.duration < logThreshold * 1.3
-        ? "yellow"
-        : e.duration < logThreshold * 1.4
-        ? "redBright"
-        : "red";
-    const dur = chalk[color](`${e.duration}ms`);
-    console.info(`prisma:query - ${dur} - ${e.query}`);
-  });
-  client
+export const prisma = remember("prisma", () =>
+  new PrismaClient()
     .$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! }))
-    .$extends(withAccelerate());
+    .$extends(withAccelerate())
+);
 
-  void client.$connect();
-  return client;
-};
-
-export const prisma = remember("prisma", prismaClientSingleton);
 export const authAdapter = PrismaAdapter(prisma);
 
 export function createSingleUser() {
