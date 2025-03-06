71b2043c841b836764ee08cf67d49bfe3102e0db
Daniel Van Cuylenburg <email@danielvanc.com>
2025-03-06 14:39:41 +0000

    feat: use next link and add prefetching

--- components/posts/PostFullView.tsx
+++ components/posts/PostFullView.tsx
@@ -2,6 +2,7 @@ import { formatDate } from "@/utils/posts";
 import { generateHTML } from "@tiptap/html";
 import StarterKit from "@tiptap/starter-kit";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function PostFullView({
   post,
@@ -23,10 +24,10 @@ export default function PostFullView({
       </time>
       <div className="group relative">
         <h3 className="mt-3 text-3xl/8 font-semibold text-gray-900 group-hover:text-gray-600">
-          <a href={`/home/posts/${post.slug}`}>
+          <Link href={`/home/posts/${post.slug}`} prefetch={true}>
             <span className="absolute inset-0" />
             {post.title}
-          </a>
+          </Link>
         </h3>
         <div
           className="mt-5 line-clamp-3 text-lg/6 text-gray-600"
