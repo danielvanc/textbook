fe3b0c5b81de083ce54e45dc314dc32973a3b149
Daniel Van Cuylenburg <email@danielvanc.com>
2025-03-07 16:58:43 +0000

    feat: cache verifyuser

--- utils/session.ts
+++ utils/session.ts
@@ -3,7 +3,9 @@ import { redirect } from "next/navigation";
 import config from "./config";
 import { type User } from "@prisma/client";
 // TODO: Add test coverage
-export async function verifyUserSession() {
+import { cache } from "react";
+
+export const verifyUserSession = cache(async () => {
   const session = await auth();
   const userId = session?.user?.id;
 
@@ -14,4 +16,4 @@ export async function verifyUserSession() {
     userId,
     user: session.user as Pick<User, "id" | "name" | "email" | "image">,
   };
-}
+});
