687da5e17fc75949685c5d7be8aa7cb7dac7d1d5
Daniel Van Cuylenburg <email@danielvanc.com>
2025-03-19 18:36:52 +0000

    feat: add tooltips

--- components/posts/bookmarks/AddBookmark.tsx
+++ components/posts/bookmarks/AddBookmark.tsx
@@ -6,6 +6,13 @@ import { useAddOptimistic } from "@/hooks/use-addOptimistic";
 import { Bookmark, BookmarkPlus } from "lucide-react";
 import Form from "next/form";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 interface BookmarkPostType {
   userId: string;
   postId: string;
@@ -16,13 +23,24 @@ export default function AddBookmark({ userId, postId }: BookmarkPostType) {
 
   return (
     <Form action={formAction}>
-      <Button className="cursor-pointer" variant={"outline"}>
-        {state.success ? (
-          <BookmarkPlus className="text-orange-500" />
-        ) : (
-          <Bookmark />
-        )}
-      </Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button className="cursor-pointer" variant={"outline"}>
+              {state.success ? (
+                <>
+                  <BookmarkPlus className="text-orange-500" />
+                </>
+              ) : (
+                <Bookmark />
+              )}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            {state.success ? <p>Remove bookmark</p> : <p>Add bookmark</p>}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <input type="hidden" name="userId" value={userId} />
       <input type="hidden" name="postId" value={postId} />
     </Form>
--- components/posts/bookmarks/RemoveBookmark.tsx
+++ components/posts/bookmarks/RemoveBookmark.tsx
@@ -2,6 +2,12 @@
 import { removeBookmark } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { useAddOptimistic } from "@/hooks/use-addOptimistic";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { BookmarkCheck, Bookmark } from "lucide-react";
 import Form from "next/form";
 
@@ -10,13 +16,22 @@ export default function RemoveBookmark({ bookmarkId }: { bookmarkId: string }) {
 
   return (
     <Form action={formAction}>
-      <Button className="cursor-pointer" variant={"outline"}>
-        {state.success ? (
-          <Bookmark />
-        ) : (
-          <BookmarkCheck className="text-orange-500" />
-        )}
-      </Button>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button className="cursor-pointer" variant={"outline"}>
+              {state.success ? (
+                <Bookmark />
+              ) : (
+                <BookmarkCheck className="text-orange-500" />
+              )}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            {state.success ? <p>Add bookmark</p> : <p>Remove bookmark</p>}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <input type="hidden" name="bookmarkId" value={bookmarkId} />
     </Form>
   );
